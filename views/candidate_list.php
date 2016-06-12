9a137a1468eb5db6addd2afdeea6ac3c02d7a675
shouko <imshouko@gmail.com>
2016-06-13 06:04:07 +0800

    link to resume in candidate list

--- views/candidate_list.php
+++ views/candidate_list.php
@@ -1,5 +1,5 @@
 <h2>Candidates</h2>
-<table class="table" style="max-width: 400px">
+<table class="table" style="max-width: 500px">
   <thead><tr><th>#</th><th>Photo</th><th>Name</th><th>Username</th><th>Action</th></tr></thead>
   <tbody>
 <?php
@@ -7,7 +7,7 @@
     echo '<tr>';
     echo '<td>'.$row['id'].'</td>';
     echo '<td><img style="border-radius: 100%; width: 100px" src="assets/pic/'.$row['user'].'.jpg"></td>';
-    echo '<td>'.$row['name'].'</td>';
+    echo '<td><a href="resume?user='.$row['user'].'">'.$row['name'].'</a></td>';
     echo '<td>'.$row['user'].'</td>';
     echo '<td><a class="btn-xs btn-warning btn" href="note?user='.$row['user'].'">Notes</a></td>';
     echo '</tr>';
